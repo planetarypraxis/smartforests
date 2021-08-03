4c860bad88b8c75cebb7b45f60a111e657d8e4f4
Chris Devereux <chris@commonknowledge.coop>
2021-08-03 14:51:41 +0000

    adds a management command to seed with fake content

--- smartforests/management/commands/seed.py
+++ smartforests/management/commands/seed.py
@@ -1,8 +1,8 @@
-from random import randint
+from random import randint, shuffle
 from django.core.management.base import BaseCommand, CommandError
 from faker import Faker, providers
 
-from logbooks.models import LogbookIndexPage, LogbookPage, StoryPage
+from logbooks.models import LogbookIndexPage, LogbookPage, StoryIndexPage, StoryPage
 
 
 class Command(BaseCommand):
@@ -16,9 +16,31 @@ class Command(BaseCommand):
         fake.add_provider(providers.lorem)
         fake.add_provider(providers.misc)
 
+        tags = [
+            fake.word()
+            for _ in range(100)
+        ]
+
         for index in LogbookIndexPage.objects.all():
             for _ in range(100):
+                shuffle(tags)
+
                 logbook = LogbookPage(title=fake.sentence())
+                logbook.tags.add(*tags[:10])
+                index.add_child(instance=logbook)
+                logbook.save()
+
+                for _ in range(randint(0, 100)):
+                    story = StoryPage(title=fake.sentence())
+                    logbook.add_child(instance=story)
+                    story.save()
+
+        for index in StoryIndexPage.objects.all():
+            for _ in range(100):
+                shuffle(tags)
+
+                logbook = StoryPage(title=fake.sentence())
+                logbook.tags.add(*tags[:10])
                 index.add_child(instance=logbook)
                 logbook.save()
 
