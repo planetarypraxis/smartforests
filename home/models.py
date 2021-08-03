aaecda72194762e5b79f2b7f703ecf96834e79cb
Jan Baykara <janbaykara@pm.me>
2021-08-03 16:45:04 +0100

    Add model and unstyled template for information pages

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from smartforests.models import CmsImage
+from django.db import models
+from wagtail.core.fields import RichTextField
 from wagtail.core.models import Page
 from commonknowledge.wagtail.models import ChildListMixin
 
@@ -6,5 +11,27 @@ class HomePage(ChildListMixin, Page):
     subpage_types = [
         'logbooks.StoryIndexPage',
         'logbooks.LogbookIndexPage',
+        'home.InformationPage'
     ]
     pass
+
+
+class InformationPage(Page):
+    parent_page_types = [
+        'home.HomePage',
+        'home.InformationPage'
+    ]
+
+    cover_image = models.ForeignKey(
+        CmsImage,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
+    text = RichTextField()
+
+    content_panels = Page.content_panels + [
+        ImageChooserPanel('cover_image'),
+        FieldPanel('text')
+    ]
