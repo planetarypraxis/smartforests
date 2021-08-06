12ba1798ffb18cd1349655b83d1e72db519551d7
Chris Devereux <chris.devereux@pm.me>
2021-08-06 16:00:21 +0000

    adds management command to regenerate indexes

---
+++ logbooks/management/commands/reindex_logbooks.py
@@ -0,0 +1,19 @@
+from logbooks.models.pages import LogbookPage, StoryPage
+from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction
+
+from logbooks.models import LogbookPageIndex
+
+
+class Command(BaseCommand):
+    help = 'Regenerate indexes for all pages in the logbook module'
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        LogbookPageIndex.objects.all().delete()
+
+        for page in StoryPage.objects.all():
+            page.save()
+
+        for page in LogbookPage.objects.all():
+            page.save()
