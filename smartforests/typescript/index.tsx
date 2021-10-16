6225c29bb6e4298a25f1195b342f36ca4445e76f
Chris Devereux <devereux.chris@gmail.com>
2021-10-16 18:21:25 +0100

    Fix mapbox environment (#11)
    * Revert "Allow process.env to be used in frontend in production builds"
    
    This reverts commit b9bccc241c4a8b651603576c09916b7b9c567aba.
    
    * Pass environment config through from django to react, removing need for webpack-driven config
    
    Co-authored-by: Chris Devereux <chris.devereux@pm.me>

--- smartforests/typescript/index.tsx
+++ smartforests/typescript/index.tsx
@@ -1,14 +1,12 @@
-console.debug("Initial page load.")
 load();
 
-document.addEventListener('turbo:load', async (event) => {
-  console.debug('TURBO page navigation', event)
+document.addEventListener("turbo:load", async (event) => {
   load();
-})
+});
 
 async function load() {
-  if (window.location.pathname.startsWith('/map')) {
-    const { main } = await import('./map');
+  if (window.location.pathname.startsWith("/map")) {
+    const { main } = await import("./map");
     main();
   }
 }
--- smartforests/typescript/map/mapbox.tsx
+++ smartforests/typescript/map/mapbox.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import MapGL, { MapContext, NavigationControl, GeolocateControl } from '@urbica/react-map-gl'
+import React from "react";
+import MapGL, {
+  MapContext,
+  NavigationControl,
+  GeolocateControl,
+} from "@urbica/react-map-gl";
 // import { WebMercatorViewport } from '@math.gl/web-mercator';
-import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
-import mapboxgl from 'mapbox-gl';
-import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import { Fragment, useContext, useEffect, useRef, useState } from 'react';
-import { AtlasPagesMapLayer } from './pages';
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+import mapboxgl from "mapbox-gl";
+import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { Fragment, useContext, useEffect, useRef, useState } from "react";
+import { AtlasPagesMapLayer } from "./pages";
+
+const MAPBOX_TOKEN = document.getElementById("MAP_APP")?.dataset.mapboxToken;
 
 export function MapVisual() {
   const [viewport, setViewport] = useState({
@@ -14,59 +20,61 @@ export function MapVisual() {
     longitude: 0,
     zoom: 2,
     bearing: 0,
-    pitch: 0
-  })
+    pitch: 0,
+  });
 
   return (
     <Fragment>
       <MapGL
-        accessToken={process.env.MAPBOX_API_PUBLIC_TOKEN}
-        mapStyle='mapbox://styles/smartforests/ckuquky9r2o3v18lkxddvri76/draft?v2'
-        style={{ width: '100%', height: '100%' }}
-        viewportChangeMethod='flyTo'
+        accessToken={MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/smartforests/ckuquky9r2o3v18lkxddvri76/draft?v2"
+        style={{ width: "100%", height: "100%" }}
+        viewportChangeMethod="flyTo"
         {...viewport}
-        onViewportChange={viewport => setViewport({
-          ...viewport,
-          // no 3D controls
-          bearing: 0,
-          pitch: 0,
-        })}
+        onViewportChange={(viewport) =>
+          setViewport({
+            ...viewport,
+            // no 3D controls
+            bearing: 0,
+            pitch: 0,
+          })
+        }
         minZoom={2}
         maxZoom={6}
       >
         {/* <NavigationControl showCompass showZoom position='top-left' /> */}
         {/* <GeolocateControl position='top-left' /> */}
-        <GeocodeControl position='top-left' accessToken={process.env.MAPBOX_API_PUBLIC_TOKEN} />
+        <GeocodeControl position="top-left" accessToken={MAPBOX_TOKEN} />
         <AtlasPagesMapLayer />
       </MapGL>
       {/* <div className='position-absolute bottom-0 end-0 me-3 mb-5 p-4 bg-white opacity-75'>
         <pre className='font-monospace mono monospace'>{JSON.stringify(results.data, null, 2)}</pre>
       </div> */}
     </Fragment>
-  )
+  );
 }
 
-function GeocodeControl({ position, accessToken }: {
-  accessToken: string,
-  position?: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right',
+function GeocodeControl({
+  position,
+  accessToken,
+}: {
+  accessToken: string;
+  position?: "top-left" | "top-right" | "bottom-left" | "bottom-right";
 }) {
-  const map: mapboxgl.Map = useContext(MapContext)
+  const map: mapboxgl.Map = useContext(MapContext);
 
   useEffect(() => {
     const control = new MapboxGeocoder({
       accessToken,
-      mapboxgl
-    })
+      mapboxgl,
+    });
 
-    map?.addControl(
-      control as any,
-      position
-    )
+    map?.addControl(control as any, position);
 
     return () => {
-      map?.removeControl(control as any)
-    }
-  }, [map, position])
+      map?.removeControl(control as any);
+    };
+  }, [map, position]);
 
-  return null
+  return null;
 }
