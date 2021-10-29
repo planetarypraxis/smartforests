eb4dcc95c92b31f29afafacce87924235c97b80b
Jan Baykara <janbaykara@pm.me>
2021-10-29 17:01:35 +0100

    Install Posthog on the back end

--- smartforests/settings/base.py
+++ smartforests/settings/base.py
@@ -265,7 +265,8 @@ WAGTAILAPI_LIMIT_MAX = None
 
 MAPBOX_API_PUBLIC_TOKEN = os.getenv('MAPBOX_API_PUBLIC_TOKEN')
 
-POSTHOG_PUBLIC_TOKEN = '...'
+POSTHOG_PUBLIC_TOKEN = None
+POSTHOG_URL = 'https://app.posthog.com'
 
 # Settings accessible via {{ settings.XXX }} in templates
 SETTINGS_EXPORT = [
