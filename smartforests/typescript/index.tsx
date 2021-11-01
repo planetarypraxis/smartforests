eb7dfe054793e876ccd216e5c3fb91bd31c5c483
Alex Andrews <alex@alexandrews.info>
2021-11-01 11:30:18 +0000

    Add simple take on updating player details

--- smartforests/typescript/index.tsx
+++ smartforests/typescript/index.tsx
@@ -18,6 +18,8 @@ async function load() {
 
   console.log(`Found ${playButtons.length} play buttons on page.`);
 
+  const radioPlayer = document.getElementById("radioPlayer");
+
   const radioPlayerAudio = document.getElementById("radioPlayerAudio");
   const radioPlayerPlayButton = document.getElementById(
     "radioPlayerPlayButton"
@@ -71,6 +73,28 @@ async function load() {
     return element;
   }
 
+  function startRadioPlayer(audioUrl, title, owner, lastPublishedAt) {
+    radioPlayerPlayButton.querySelector(".play-button").classList.add("d-none");
+    radioPlayerPlayButton
+      .querySelector(".pause-button")
+      .classList.remove("d-none");
+
+    radioPlayer.querySelector(
+      "[data-smartforests-radio-episode-title]"
+    ).innerHTML = title;
+    radioPlayer.querySelector(
+      "[data-smartforests-radio-episode-owner]"
+    ).innerHTML = owner;
+    radioPlayer.querySelector(
+      "[data-smartforests-radio-episode-last-published-at]"
+    ).innerHTML = lastPublishedAt;
+
+    radioPlayerAudio.src = audioUrl;
+    radioPlayerAudio.play();
+
+    radioIsPlaying = true;
+  }
+
   Array.from(playButtons).forEach((playButton) => {
     playButton.addEventListener("click", (event) => {
       event.stopImmediatePropagation();
@@ -91,13 +115,15 @@ async function load() {
       }
 
       const audioUrl = buttonElement.dataset.smartforestsAudio;
+      const title = buttonElement.dataset.smartforestsTitle;
+      const lastPublishedAt = buttonElement.dataset.smartforestsLastPublishedAt;
+      const owner = buttonElement.dataset.smartforestsOwner;
 
       buttonElement.querySelector(".play-button").classList.add("d-none");
       buttonElement.querySelector(".pause-button").classList.remove("d-none");
 
       Array.from(playButtons).forEach((otherPlaybutton) => {
         if (otherPlaybutton === buttonElement) {
-          console.log("Our element, skipping");
           return;
         }
 
@@ -109,19 +135,9 @@ async function load() {
 
       console.log(`Loading ${audioUrl}`);
 
-      radioPlayerAudio.src = audioUrl;
-
       radioPlayerOffCanvas.show();
-      radioPlayerAudio.play();
 
-      radioPlayerPlayButton
-        .querySelector(".play-button")
-        .classList.add("d-none");
-      radioPlayerPlayButton
-        .querySelector(".pause-button")
-        .classList.remove("d-none");
-
-      radioIsPlaying = true;
+      startRadioPlayer(audioUrl, title, owner, lastPublishedAt);
     });
   });
 }
