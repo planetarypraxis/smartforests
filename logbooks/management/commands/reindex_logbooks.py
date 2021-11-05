da731eeb03f50dd428e1cb4b9da9089d8fe899c9
Chris Devereux <devereux.chris@gmail.com>
2021-11-05 17:35:14 +0000

    Make filtering & index pages consistent (#42)
    * combine approaches to filtering across all list pages
    * ensure filter bar fills the page when there aren't many tags
    * remove now unneeded (and confusing) LogbookPageIndex
    * actually store the generated thumbnails in thumbnailmixin
    * don't cache the list of relevant tags

--- logbooks/management/commands/reindex_logbooks.py
+++ logbooks/management/commands/reindex_logbooks.py
@@ -1,8 +1,7 @@
-from logbooks.models.pages import LogbookPage, LogbookEntryPage
+from logbooks.models.pages import LogbookPage, LogbookEntryPage, StoryPage
 from django.core.management.base import BaseCommand, CommandError
 from django.db import transaction
 
-from logbooks.models import LogbookPageIndex
 from logbooks.models.tag_cloud import TagCloud
 from smartforests.models import Tag
 
@@ -12,12 +11,13 @@ class Command(BaseCommand):
 
     @transaction.atomic
     def handle(self, *args, **options):
-        LogbookPageIndex.objects.all().delete()
-
         for page in LogbookEntryPage.objects.all():
             page.save()
 
         for page in LogbookPage.objects.all():
+            page.save()
+
+        for page in StoryPage.objects.all():
             page.save()
 
         Tag.regenerate_thumbnails()
