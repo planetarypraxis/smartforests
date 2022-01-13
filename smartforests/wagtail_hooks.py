17f4bf1aedb66ebebec7f0d0d980dfa6d20a207b
Jan Baykara <janbaykara@pm.me>
2022-01-13 11:51:01 +0000

    Also hide 'delete' from the dropdown menu in listings, for non-superusers

--- smartforests/wagtail_hooks.py
+++ smartforests/wagtail_hooks.py
@@ -1,10 +1,12 @@
+from django.utils.functional import cached_property
 from wagtail.core import hooks
+from wagtail.admin.widgets.button import ButtonWithDropdownFromHook
 
 
 @hooks.register('construct_page_action_menu')
 def customise_page_actions(menu_items, request, context):
     '''
-    Disallow non-admins from deleting pages
+    Disallow non-admins from deleting pages via the Page Editor
     '''
     if not request.user.is_superuser:
         try:
@@ -16,7 +18,7 @@ def customise_page_actions(menu_items, request, context):
             pass
 
     '''
-    Make 'publish' the default action in the editor UI.
+    Make 'publish' the default action in Page Editor
     '''
     try:
         for (index, item) in enumerate(menu_items):
@@ -26,3 +28,41 @@ def customise_page_actions(menu_items, request, context):
                 break
     except:
         pass
+
+
+@hooks.register('construct_page_listing_buttons')
+def remove_page_listing_button_item(buttons, page, page_perms, is_parent=False, context=None):
+    '''
+    Disallow non-admins from deleting pages via the listing
+    '''
+    if not context.request.user.is_superuser:
+        more_actions_dropdown = buttons.pop()
+        buttons.append(ButtonWithDropdownFromHookExcludingDelete(
+            more_actions_dropdown))
+    return buttons
+
+
+class ButtonWithDropdownFromHookExcludingDelete(ButtonWithDropdownFromHook):
+    template_name = 'wagtailadmin/pages/listing/_button_with_dropdown.html'
+
+    def __init__(self, base_class, **kwargs):
+        self.hook_name = base_class.hook_name
+        self.page = base_class.page
+        self.page_perms = base_class.page_perms
+        self.is_parent = base_class.is_parent
+        self.next_url = base_class.next_url
+
+        super().__init__(base_class.label, hook_name=base_class.hook_name, page=base_class.page,
+                         page_perms=base_class.page_perms, is_parent=base_class.is_parent, next_url=base_class.next_url, **kwargs)
+
+    @cached_property
+    def dropdown_buttons(self):
+        buttons = super().dropdown_buttons
+        try:
+            for (index, item) in enumerate(buttons):
+                if item.label == 'Delete':
+                    buttons.pop(index)
+                    break
+        except:
+            pass
+        return buttons
