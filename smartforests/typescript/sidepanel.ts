2f7c6ac5a88b1eb8bf974c387c0e3cfb1828d7ce
Jan Baykara <janbaykara@pm.me>
2022-02-09 17:59:25 +0000

    Fix event listeners for turbo-frame loaded metadata template

--- smartforests/typescript/sidepanel.ts
+++ smartforests/typescript/sidepanel.ts
@@ -1,20 +1,29 @@
 const init = () => {
+  // Clean up
   document
     .querySelectorAll<HTMLElement>("[data-smartforests-sidepanel-open]")
     .forEach((el) => {
-      const sidepanel = document.querySelector(
-        el.dataset.smartforestsSidepanelOpen
-      );
-
-      el.addEventListener("click", () => {
-        if (sidepanel) {
-          const instance =
-            bootstrap.Offcanvas.getInstance(sidepanel) ||
-            new bootstrap.Offcanvas(sidepanel);
-          instance.show();
-        }
-      });
+      el.removeEventListener("click", onClickSidepanelLink)
+    })
+  // Set up
+  document
+    .querySelectorAll<HTMLElement>("[data-smartforests-sidepanel-open]")
+    .forEach((el) => {
+      el.addEventListener("click", onClickSidepanelLink);
     });
 };
 
+function onClickSidepanelLink(e) {
+  const sidepanel = document.querySelector(
+    e.target.dataset.smartforestsSidepanelOpen
+  );
+  if (sidepanel) {
+    const instance =
+      bootstrap.Offcanvas.getInstance(sidepanel) ||
+      new bootstrap.Offcanvas(sidepanel);
+    instance.show();
+  }
+}
+
 window.addEventListener("turbo:load", init);
+window.addEventListener("turbo:frame-render", init)
