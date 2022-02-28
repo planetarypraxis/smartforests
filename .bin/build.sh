454b3a54e5ee0dc125c21295af01398619fa1678
Alex Andrews <alex@alexandrews.info>
2022-02-28 16:45:52 +0000

    Make logging more verbose

--- .bin/build.sh
+++ .bin/build.sh
@@ -4,6 +4,7 @@ yarn build
 rm -rf node_modules
 
 if [ -z "$MEDIA_URL" ]; then
+    echo "MEDIA_URL environment variable is blank, setting to https://smartforests.ams3.cdn.digitaloceanspaces.com/"
     export MEDIA_URL=https://smartforests.ams3.cdn.digitaloceanspaces.com/
 fi
 
