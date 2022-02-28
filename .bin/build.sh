4155f051a2ffa811aecd1719602ae3cb6ed3a82d
Alex Andrews <alex@alexandrews.info>
2022-02-28 16:44:54 +0000

    Check for existence of MEDIA_URL environment variable and set if not present

--- .bin/build.sh
+++ .bin/build.sh
@@ -2,5 +2,9 @@ set -e
 
 yarn build
 rm -rf node_modules
-echo "MEDIA_URL set to $MEDIA_URL"
+
+if [ -z "$MEDIA_URL" ]; then
+    export MEDIA_URL=https://smartforests.ams3.cdn.digitaloceanspaces.com/
+fi
+
 SKIP_DB=1 SECRET_KEY=dummy python manage.py collectstatic --noinput --clear
