216c111546968e15b484a516e2d94650cabdaedd
Alex Andrews <alex@alexandrews.info>
2022-02-28 18:18:17 +0000

    Disable pages middleware but trust Git

--- smartforests/settings/base.py
+++ smartforests/settings/base.py
@@ -97,7 +97,6 @@ MIDDLEWARE = [
 
     'django.middleware.locale.LocaleMiddleware',
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
-    # 'logbooks.middleware.pages.ImportantPagesMiddleware',
 ]
 
 ROOT_URLCONF = 'smartforests.urls'
