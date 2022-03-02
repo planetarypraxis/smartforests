4cdaf7709c9e58c0255ee58ec63a54593a7f59cf
Jan Baykara <janbaykara@pm.me>
2022-03-02 17:44:00 +0000

    Move i18n processor to the top of the context stack, above wagtailmenus

--- smartforests/settings/base.py
+++ smartforests/settings/base.py
@@ -111,15 +111,15 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.i18n',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'wagtail.contrib.settings.context_processors.settings',
-                'wagtailmenus.context_processors.wagtailmenus',
                 'django_settings_export.settings_export',
-                'django.template.context_processors.i18n',
                 "wagtail.contrib.settings.context_processors.settings",
+                'wagtailmenus.context_processors.wagtailmenus',
             ],
         },
     },
