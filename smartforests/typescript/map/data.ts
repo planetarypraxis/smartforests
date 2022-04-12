a2d06b11d37b1fc5a10447a9892a73e58d1c163e
Jan Baykara <janbaykara@pm.me>
2022-04-12 13:30:17 +0100

    Don't use superclusterd, just cluster locally

--- smartforests/typescript/map/data.ts
+++ smartforests/typescript/map/data.ts
@@ -1,9 +1,13 @@
-import { RefObject, useLayoutEffect, useMemo, useState } from "react";
+import { RefObject, useEffect, useLayoutEffect, useMemo, useRef, useState } from "react";
 import useResizeObserver from "@react-hook/resize-observer";
-import { Superclusterd, Viewport } from "superclusterd";
+import { ClusteredFeature, Superclusterd, Viewport } from "superclusterd";
 import { useClusteredMapData } from "superclusterd/react";
 import { SmartForest } from "./types";
 import { stringifyQuery } from "./state";
+import Supercluster from 'supercluster'
+import useSWR from "swr";
+import WebMercatorViewport from "@math.gl/web-mercator";
+import { BBox, FeatureCollection, Point } from "geojson";
 
 export type MapViewport = Viewport;
 
@@ -17,12 +21,73 @@ export const useFeatures = <T>(
   getQuery: () => any,
   deps: unknown[] = []
 ) =>
-  useClusteredMapData<SmartForest.MapItem>(
-    supercluster,
+  useClusteredMapDataLocal<SmartForest.MapItem>(
     dimensions,
     viewport,
     useMemo(() => getFeaturesUrl(getQuery()), deps)
   );
+
+
+function useClusteredMapDataLocal<T>(
+  dimensions: {
+    width: number
+    height: number
+  },
+  viewport: Viewport,
+  url: string
+): ClusteredFeature<T>[] {
+  const [state, setState] = useState<ClusteredFeature<T>[]>([])
+
+  const data = useSWR(
+    url,
+    async (url) => {
+      const data: FeatureCollection<Point, T> = await fetch(url).then((response) => response.json())
+      const supercluster = new Supercluster<T>({
+        map: (props) => {
+          return {
+            features: [props]
+          }
+        },
+        reduce: (accumulated, props) => {
+          accumulated.features = [...accumulated.features, ...props.features]
+        },
+      });
+      supercluster.load(data.features)
+      return {
+        supercluster,
+        data
+      }
+    },
+    { revalidateOnFocus: false, revalidateOnReconnect: false }
+  );
+
+  useEffect(() => {
+    // Calculate window
+    const projection = new WebMercatorViewport({
+      width: dimensions.width,
+      height: dimensions.height,
+      latitude: viewport.latitude,
+      longitude: viewport.longitude,
+      zoom: viewport.zoom,
+    });
+
+    const bounds = projection.getBounds();
+    const bbox = [...bounds[0], ...bounds[1]] as any
+
+    // Generate clusters
+    const clusters = data.data?.supercluster?.getClusters(bbox, projection.zoom)
+    setState(clusters)
+  }, [
+    data.data,
+    dimensions.width,
+    dimensions.height,
+    viewport.latitude,
+    viewport.longitude,
+    viewport.zoom,
+  ])
+
+  return state
+}
 
 export const useSize = (target: RefObject<HTMLElement>) => {
   const [size, setSize] = useState<DOMRectReadOnly>();
@@ -42,5 +107,5 @@ const getFeaturesUrl = (opts: { tag?: string, languageCode?: string }) => {
     ...(opts.languageCode ? { language_code: opts.languageCode } : {}),
   });
 
-  return `${window.location.host}/api/v2/geo/${q}`;
+  return `${window.location.protocol}//${window.location.host}/api/v2/geo/${q}`;
 };
