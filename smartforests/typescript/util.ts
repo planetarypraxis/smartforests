e8bfb7e82f7446c8dc26826d5ab337950a5ef230
Jan Baykara <janbaykara@pm.me>
2022-08-10 17:33:25 +0100

    Fix time duration displayed for the active radio player

--- smartforests/typescript/util.ts
+++ smartforests/typescript/util.ts
@@ -11,7 +11,7 @@ const ONE_HOUR_IN_SECONDS = 60 * 60
 
 export function formatDuration(seconds: number): string {
   var date = new Date()
-  date.setUTCHours(0, 0, 0, 0)
+  date.setHours(0, 0, 0, 0)
   date.setSeconds(seconds);
   var timeString = date.toTimeString().split(" ")[0]
   if (seconds < ONE_HOUR_IN_SECONDS) {
