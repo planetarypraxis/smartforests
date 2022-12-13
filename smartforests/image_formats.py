8b94100b8163f74fb3cd98b571d11bd91113f06c
lowlandjuju <lowlandjuju@users.noreply.github.com>
2022-12-13 16:14:19 +0000

    Target all richtext with responsive style

--- smartforests/image_formats.py
+++
@@ -1,4 +0,0 @@
-from wagtail.images.formats import Format, register_image_format
-
-register_image_format(Format('responsive', 'Responsive',
-                      'richtext-image responsive', 'original'))
