66c1fde2ecf21455e5d3b8ba58b150a1fbed5906
Jan Baykara <janbaykara@pm.me>
2023-03-06 15:19:31 +0000

    Simply disable the sync tree function

--- smartforests/wagtail_localize_tasks.py
+++ smartforests/wagtail_localize_tasks.py
@@ -1,12 +1,11 @@
 from wagtail_localize.tasks import BaseJobBackend
 from django.conf import settings
-from smartforests.queue import create_serialized_job
 
 
 class DjangoDBQueueJobBackend(BaseJobBackend):
     def enqueue(self, func, args, kwargs):
         if settings.USE_BACKGROUND_WORKER and func.__name__ == 'synchronize_tree':
-            create_serialized_job(
-                "wagtail_localize.synctree.synchronize_tree", args, kwargs)
+            print("Disabled syncing")
+            pass
         else:
             func(*args, **kwargs)
