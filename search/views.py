fa1874626b06670b626ca8d0241ec84766afe614
Jan Baykara <janbaykara@pm.me>
2023-09-14 13:27:04 +0100

    Translate tags if possible

--- search/views.py
+++ search/views.py
@@ -10,6 +10,13 @@ class SearchView(BasicSearchView, LocaleFromLanguageCode):
         res = super().search_queryset(qs, search_query)
         results = list(res) + list(qs.autocomplete(search_query))
         locale = self.get_locale()
+        self.request.LANGUAGE_CODE = locale.language_code
         localized_pages = list(
             set([page.get_translation_or_none(locale).specific or page for page in results]))
         return localized_pages
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        locale = self.get_locale()
+        context["LANGUAGE_CODE"] = locale.language_code
+        return context
