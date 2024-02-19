a210578d94b2d7308030e304169552965dc1a43b
Joaquim d'Souza <joaquimds@gmail.com>
2024-02-19 11:26:45 +0000

    fix: remove_page_listing_button_item for non superadmins

--- smartforests/wagtail_hooks.py
+++ smartforests/wagtail_hooks.py
@@ -8,26 +8,26 @@ from django.utils.html import format_html
 from django.templatetags.static import static
 
 
-@hooks.register('construct_page_action_menu')
+@hooks.register("construct_page_action_menu")
 def customise_page_actions(menu_items, request, context):
-    '''
+    """
     Disallow non-admins from deleting pages via the Page Editor
-    '''
+    """
     if not request.user.is_superuser:
         try:
-            for (index, item) in enumerate(menu_items):
-                if item.name == 'action-delete':
+            for index, item in enumerate(menu_items):
+                if item.name == "action-delete":
                     menu_items.pop(index)
                     break
         except:
             pass
 
-    '''
+    """
     Make 'publish' the default action in Page Editor
-    '''
+    """
     try:
-        for (index, item) in enumerate(menu_items):
-            if item.name == 'action-publish' or item.name == 'action-submit':
+        for index, item in enumerate(menu_items):
+            if item.name == "action-publish" or item.name == "action-submit":
                 menu_items.pop(index)
                 menu_items.insert(0, item)
                 break
@@ -35,20 +35,20 @@ def customise_page_actions(menu_items, request, context):
         pass
 
 
-@hooks.register('construct_page_listing_buttons')
+@hooks.register("construct_page_listing_buttons")
 def remove_page_listing_button_item(buttons, page, page_perms, context=None):
-    '''
+    """
     Disallow non-admins from deleting pages via the listing
-    '''
-    if not context.request.user.is_superuser:
+    """
+    is_superuser = "request" in context and context["request"].user.is_superuser
+    if not is_superuser:
         more_actions_dropdown = buttons.pop()
-        buttons.append(ButtonWithDropdownFromHookExcludingDelete(
-            more_actions_dropdown))
+        buttons.append(ButtonWithDropdownFromHookExcludingDelete(more_actions_dropdown))
     return buttons
 
 
 class ButtonWithDropdownFromHookExcludingDelete(ButtonWithDropdownFromHook):
-    template_name = 'wagtailadmin/pages/listing/_button_with_dropdown.html'
+    template_name = "wagtailadmin/pages/listing/_button_with_dropdown.html"
 
     def __init__(self, base_class, **kwargs):
         self.hook_name = base_class.hook_name
@@ -56,15 +56,21 @@ class ButtonWithDropdownFromHookExcludingDelete(ButtonWithDropdownFromHook):
         self.page_perms = base_class.page_perms
         self.next_url = base_class.next_url
 
-        super().__init__(base_class.label, hook_name=base_class.hook_name, page=base_class.page,
-                         page_perms=base_class.page_perms, next_url=base_class.next_url, **kwargs)
+        super().__init__(
+            base_class.label,
+            hook_name=base_class.hook_name,
+            page=base_class.page,
+            page_perms=base_class.page_perms,
+            next_url=base_class.next_url,
+            **kwargs
+        )
 
     @cached_property
     def dropdown_buttons(self):
         buttons = super().dropdown_buttons
         try:
-            for (index, item) in enumerate(buttons):
-                if item.label == 'Delete':
+            for index, item in enumerate(buttons):
+                if item.label == "Delete":
                     buttons.pop(index)
                     break
         except:
@@ -72,9 +78,11 @@ class ButtonWithDropdownFromHookExcludingDelete(ButtonWithDropdownFromHook):
         return buttons
 
 
-@hooks.register('register_admin_viewset')
+@hooks.register("register_admin_viewset")
 def register_person_chooser_viewset():
-    return RadioEpisodeChooserViewSet('radio_episode_chooser', url_prefix='radio-episode-chooser')
+    return RadioEpisodeChooserViewSet(
+        "radio_episode_chooser", url_prefix="radio-episode-chooser"
+    )
 
 
 @hooks.register("insert_global_admin_css")
@@ -88,12 +96,11 @@ def insert_global_admin_css():
 @hooks.register("insert_global_admin_js", order=100)
 def global_admin_js():
     return format_html(
-        '<script type="module" src="{}"></script>',
-        static("js/radio_wagtailmedia.js")
+        '<script type="module" src="{}"></script>', static("js/radio_wagtailmedia.js")
     )
 
 
-@hooks.register('after_publish_page')
+@hooks.register("after_publish_page")
 def after_publish_page_update_translations(request, page):
     """Update translations when original page is saved."""
     if hasattr(page, "translation_key"):
@@ -101,7 +108,8 @@ def after_publish_page_update_translations(request, page):
         TranslationSource.update_or_create_from_instance(page)
 
         translated_pages = Page.objects.filter(
-            translation_key=page.translation_key).exclude(id=page.id)
+            translation_key=page.translation_key
+        ).exclude(id=page.id)
         for translated_page in translated_pages:
             # Update each translation with the new synched field values
             translation = Translation.objects.filter(
