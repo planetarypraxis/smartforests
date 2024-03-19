f5cd1057872c2438c377be8f825876085a69d798
joaquimds <joaquimds@gmail.com>
2024-03-19 11:18:19 +0000

    feat: upgrade to wagtail 5 (#296)
    * feat: upgradte to wagtail 5
    
    * fix: remove_page_listing_button_item for non superadmins
    
    * fix: admin autocomplete font color

--- logbooks/models/fields.py
+++ logbooks/models/fields.py
@@ -7,9 +7,12 @@ from smartforests.models import Tag
 class TagFieldWidget(AdminTagWidget):
     def get_context(self, name, value, attrs):
         context = super().get_context(name, value, attrs)
-        if self.attrs['locale']:
-            context['widget']['autocomplete_url'] = context['widget']['autocomplete_url'] + \
-                "?locale=" + self.attrs["locale"]
+        if self.attrs["locale"]:
+            context["widget"]["attrs"]["data-w-tag-url-value"] = (
+                context["widget"]["attrs"]["data-w-tag-url-value"]
+                + "?locale="
+                + self.attrs["locale"]
+            )
         return context
 
 
@@ -25,10 +28,13 @@ class TagFieldPanel(FieldPanel):
     class BoundPanel(FieldPanel.BoundPanel):
         def __init__(self, **kwargs):
             super().__init__(**kwargs)
-            locale = self.form.instance.locale.language_code if self.form.instance and self.form.instance.locale else None
-            tag_field = self.form.fields['tags']
-            tag_field.widget.attrs['locale'] = locale
-
+            locale = (
+                self.form.instance.locale.language_code
+                if self.form.instance and self.form.instance.locale
+                else None
+            )
+            tag_field = self.form.fields["tags"]
+            tag_field.widget.attrs["locale"] = locale
 
 
 class LocalizedTaggableManager(ClusterTaggableManager):
