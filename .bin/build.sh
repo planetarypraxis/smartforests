96e910e25d8c40bc3158545ab78a6617482b134a
Joaquim d'Souza <joaquimds@gmail.com>
2024-03-21 13:35:14 +0000

    feat: remove build from dockerfile, move to predeploy command

--- .bin/build.sh
+++ .bin/build.sh
@@ -8,4 +8,4 @@ if [ -z "$MEDIA_URL" ]; then
     export MEDIA_URL=https://smartforests.ams3.cdn.digitaloceanspaces.com/
 fi
 
-SKIP_DB=1 SECRET_KEY=dummy python manage.py collectstatic --noinput --clear
+SKIP_DB=1 python manage.py collectstatic --noinput
