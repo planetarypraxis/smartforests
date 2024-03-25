e6e97b9c3c94339662c0581c8c2aa39fd74e7dac
Joaquim d'Souza <joaquimds@gmail.com>
2024-03-25 12:50:53 +0100

    feat: improve tag cloud style and ux

--- smartforests/static/js/tag_cloud.js
+++ smartforests/static/js/tag_cloud.js
@@ -14,6 +14,7 @@ document.querySelectorAll("[data-tag-cloud-data]").forEach(($parent) => {
   if (tagOffCanvasId) {
     tagOffcanvas = document.getElementById(tagOffCanvasId);
   }
+  const initialOffCanvasContent = tagOffcanvas?.innerHTML || "";
 
   // Specify the dimensions of the chart.
   const { width, height } = calculateCanvasDimensions();
@@ -22,11 +23,25 @@ document.querySelectorAll("[data-tag-cloud-data]").forEach(($parent) => {
   const $container = document.createElement("div");
   $parent.appendChild($container);
 
-  doTagCloud($container, width, height, data, tagOffcanvas);
+  doTagCloud(
+    $container,
+    width,
+    height,
+    data,
+    tagOffcanvas,
+    initialOffCanvasContent
+  );
 
   window.addEventListener("resize", () => {
     const { width, height } = calculateCanvasDimensions();
-    doTagCloud($container, width, height, data, tagOffcanvas);
+    doTagCloud(
+      $container,
+      width,
+      height,
+      data,
+      tagOffcanvas,
+      initialOffCanvasContent
+    );
   });
 });
 
@@ -43,18 +58,33 @@ function calculateCanvasDimensions() {
   return { width, height };
 }
 
-function doTagCloud($container, width, height, data, tagOffcanvas) {
+function doTagCloud(
+  $container,
+  width,
+  height,
+  data,
+  tagOffcanvas,
+  initialOffCanvasContent
+) {
   $container.innerHTML = "";
 
-  let focusedNodeId = null;
+  let focusedNode = null;
 
   // Specify the color scale.
-  const color = d3.scaleSequential(d3.interpolateGreens);
+  const interpolateColors = d3.interpolateRgbBasis([
+    "#C7EAC6",
+    "#71B340",
+    "#026302",
+  ]);
+
+  const color = d3.scaleSequential(interpolateColors);
 
   // The force simulation mutates links and nodes, so create a copy
   // so that re-evaluating this function
   const links = data.links.map((d) => ({ ...d }));
   const nodes = data.nodes.map((d) => ({ ...d }));
+  nodes.sort(() => (Math.random() < 0.5 ? -1 : 1));
+
   const maxCount = data.max_count;
 
   // Create a simulation with several forces.
@@ -80,10 +110,10 @@ function doTagCloud($container, width, height, data, tagOffcanvas) {
 
   const context = canvas.getContext("2d");
   context.scale(dpi, dpi);
+  context.font = "12px Monaco";
 
   function draw() {
     context.clearRect(0, 0, width, height);
-    context.font = "12px Arial";
 
     drawBackground();
     drawNodes(nodes);
@@ -128,20 +158,6 @@ function doTagCloud($container, width, height, data, tagOffcanvas) {
     context.restore();
   }
 
-  function drawLinks(links) {
-    // Draw lines
-    context.save();
-    context.globalAlpha = 0.6;
-    context.strokeStyle = "#999";
-    context.beginPath();
-    links.forEach((d) => {
-      context.moveTo(d.source.x, d.source.y);
-      context.lineTo(d.target.x, d.target.y);
-    });
-    context.stroke();
-    context.restore();
-  }
-
   function drawNodes(nodes) {
     const padding = 8;
 
@@ -149,100 +165,91 @@ function doTagCloud($container, width, height, data, tagOffcanvas) {
     context.strokeStyle = "#fff";
     context.globalAlpha = 1;
 
-    let focusedNode = null;
     nodes.forEach((node) => {
       drawNode(node);
-      if (node.id === focusedNodeId) {
-        focusedNode = node;
-      }
     });
 
     // Keep track of expanded nodes to prevent overlap
     const expandedNodes = {};
-    const covered = [];
-
-    // Start with focused node as this should always be shown
-    // if (focusedNode) {
-    //   expandedNodes[focusedNodeId] = true;
-    //   const text = focusedNode.name.toUpperCase();
-    //   const textWidth = context.measureText(text).width;
-    //   covered.push({
-    //     left: focusedNode.x - padding,
-    //     right: focusedNode.x + textWidth + padding,
-    //     top: focusedNode.y - padding,
-    //     bottom: focusedNode.y + padding,
-    //   });
-    // }
-
+    const overlapPadding = 32;
     nodes.forEach((node) => {
       const text = node.name.toUpperCase();
       const textWidth = context.measureText(text).width;
 
-      let nodeBounds;
+      node.top = node.y - padding;
+      node.bottom = node.y + padding;
+
       if (node.x + textWidth + padding <= width) {
-        nodeBounds = {
-          left: node.x - padding,
-          right: node.x + textWidth + padding,
-          top: node.y - padding,
-          bottom: node.y + padding,
-        };
+        node.left = node.x - padding * 2;
+        node.right = node.x + textWidth + padding * 2;
       } else {
-        nodeBounds = {
-          left: node.x - padding - textWidth,
-          right: node.x + padding,
-          top: node.y - padding,
-          bottom: node.y + padding,
-        };
+        node.left = node.x - textWidth - padding * 2;
+        node.right = node.x + padding + padding * 2;
       }
 
-      const overlaps = covered.filter((c) => {
+      node.overlapLeft = node.left - overlapPadding;
+      node.overlapRight = node.right + overlapPadding;
+      node.overlapTop = node.y - overlapPadding;
+      node.overlapBottom = node.y + overlapPadding;
+
+      const overlaps = Object.keys(expandedNodes).filter((id) => {
+        const expandedNode = expandedNodes[id];
         if (
-          nodeBounds.right >= c.left &&
-          nodeBounds.left <= c.right &&
-          nodeBounds.bottom >= c.top &&
-          nodeBounds.top <= c.bottom
+          node.overlapRight >= expandedNode.overlapLeft &&
+          node.overlapLeft <= expandedNode.overlapRight &&
+          node.overlapBottom >= expandedNode.overlapTop &&
+          node.overlapTop <= expandedNode.overlapBottom
         ) {
           return true;
         }
       });
 
       if (!overlaps.length) {
-        expandedNodes[node.id] = true;
+        expandedNodes[node.id] = node;
+        node.expanded = true;
         // Draw focused node at the end
-        if (node.id !== focusedNodeId) {
+        if (node.id !== focusedNode?.id) {
           drawLabelContainer(node.x, node.y, textWidth, padding);
           drawLabel(node.x, node.y, text, textWidth, padding);
         }
-        covered.push(nodeBounds);
+      } else {
+        node.expanded = false;
       }
     });
 
     nodes.forEach((node) => {
       // Make sure node circle is on top of the label
-      // Draw focused node at the end
-      if (expandedNodes[node.id] && node.id !== focusedNodeId) {
+      // Draw focused node at the end (below)
+      if (expandedNodes[node.id] && node.id !== focusedNode?.id) {
         drawNode(node);
       }
     });
 
     if (focusedNode) {
       // Make sure focused node is on top of everything
+      focusedNode.expanded = true;
       const text = focusedNode.name.toUpperCase();
       const textWidth = context.measureText(text).width;
-      drawLabelContainer(focusedNode.x, focusedNode.y, textWidth, padding);
-      drawLabel(focusedNode.x, focusedNode.y, text, textWidth, padding);
+      drawLabelContainer(
+        focusedNode.x,
+        focusedNode.y,
+        textWidth,
+        padding,
+        true
+      );
+      drawLabel(focusedNode.x, focusedNode.y, text, textWidth, padding, true);
       drawNode(focusedNode);
     }
 
     context.restore();
   }
 
-  function drawLabelContainer(x, y, textWidth, padding) {
+  function drawLabelContainer(x, y, textWidth, padding, invert = false) {
     context.save();
 
     context.strokeStyle = "#fff";
     context.globalAlpha = 0.8;
-    context.fillStyle = color(1);
+    context.fillStyle = invert ? "#043003" : "#fff";
 
     context.beginPath();
 
@@ -280,6 +287,7 @@ function doTagCloud($container, width, height, data, tagOffcanvas) {
 
   function drawNode(node) {
     context.globalAlpha = 1;
+    context.strokeStyle = "#fff";
     context.fillStyle = color(1 - node.count / maxCount);
     context.beginPath();
     context.moveTo(node.x + 5, node.y);
@@ -288,8 +296,8 @@ function doTagCloud($container, width, height, data, tagOffcanvas) {
     context.stroke();
   }
 
-  function drawLabel(x, y, text, textWidth, padding) {
-    context.fillStyle = color(0);
+  function drawLabel(x, y, text, textWidth, padding, invert = false) {
+    context.fillStyle = invert ? "#fff" : "#026302";
     if (x + textWidth + padding <= width) {
       context.fillText(text, x + 10, y + 4);
     } else {
@@ -335,43 +343,45 @@ function doTagCloud($container, width, height, data, tagOffcanvas) {
     event.subject.fy = null;
   }
 
-  function showLabel(text, x, y) {
-    const label = document.getElementById("label");
-    label.innerText = text;
-    label.style.left = x;
-    label.style.top = y;
-    label.style.position = "absolute";
-    label.style.display = "block";
-  }
+  const findFocusedNode = (px, py) => {
+    for (const node of nodes) {
+      if (
+        node.expanded &&
+        node.left <= px &&
+        node.right >= px &&
+        node.top <= py &&
+        node.bottom >= py
+      ) {
+        return node;
+      }
+    }
 
-  function hideLabel() {
-    const label = document.getElementById("label");
-    label.style.display = "none";
-  }
-
-  canvas.addEventListener("mousemove", (event) => {
-    const [px, py] = d3.pointer(event, canvas);
-    const node = d3.least(nodes, ({ x, y }) => {
+    return d3.least(nodes, ({ x, y }) => {
       const dist2 = (x - px) ** 2 + (y - py) ** 2;
       if (dist2 < 400) return dist2;
     });
-    if (node) {
+  };
+
+  canvas.addEventListener("mousemove", (event) => {
+    const [px, py] = d3.pointer(event, canvas);
+
+    let hoveredNode = findFocusedNode(px, py);
+    if (hoveredNode) {
       canvas.style.cursor = "pointer";
-      focusedNodeId = node.id;
+      focusedNode = hoveredNode;
     } else {
       canvas.style.removeProperty("cursor");
-      focusedNodeId = null;
+      focusedNode = null;
     }
     draw();
   });
 
   canvas.addEventListener("click", (event) => {
     const [px, py] = d3.pointer(event, canvas);
-    const node = d3.least(nodes, ({ x, y }) => {
-      const dist2 = (x - px) ** 2 + (y - py) ** 2;
-      if (dist2 < 400) return dist2;
-    });
+    const node = findFocusedNode(px, py);
     if (node && tagOffcanvas) {
+      // Show loading indicator
+      tagOffcanvas.innerHTML = initialOffCanvasContent;
       window.Turbo.visit(`/${getLanguageCode()}/_tags/${node.slug}/`, {
         frame: "tagpanel-turboframe",
       });
