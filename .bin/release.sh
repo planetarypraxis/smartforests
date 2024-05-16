5bf69ab1ccbc06bcec9313333b8e0c83fd8aeb95
ev-sc <4164774+ev-sc@users.noreply.github.com>
2024-05-16 19:31:52 +0200

    Re-institute migration on release

--- .bin/release.sh
+++ .bin/release.sh
@@ -1,6 +1,5 @@
 set -e
-
-#pipenv run python manage.py migrate
-#pipenv run python manage.py setup_pages
+ 
+pipenv run python manage.py migrate
 pipenv run python manage.py createcachetable
 pipenv run python manage.py collectstatic --noinput
