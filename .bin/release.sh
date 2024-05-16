19b72918eb1229505f4358a4bc8254021e0001bb
ev-sc <4164774+ev-sc@users.noreply.github.com>
2024-05-16 10:27:00 +0200

    Remove release command from dockerfile

--- .bin/release.sh
+++ .bin/release.sh
@@ -3,3 +3,4 @@ set -e
 #pipenv run python manage.py migrate
 #pipenv run python manage.py setup_pages
 pipenv run python manage.py createcachetable
+pipenv run python manage.py collectstatic --noinput
