cb905d5250614a464524d57176c9ad6a6444162f
ev-sc <4164774+ev-sc@users.noreply.github.com>
2024-05-21 21:06:17 +0200

    Add cache warming task to worker instance

--- logbooks/tasks.py
+++ logbooks/tasks.py
@@ -1,6 +1,7 @@
 from django.core.management import call_command
 from wagtail.models import Page
 from background_task import background
+import requests, os, time
 
 from smartforests.models import Tag
 from smartforests.tag_cloud import recalculate_taglinks
@@ -38,3 +39,58 @@ def regenerate_tag_cloud(tag_id: int):
 def publish():
     print(f"Publishing scheduled pages")
     call_command('publish_scheduled_pages')
+
+@background(schedule=5, remove_existing_tasks=True)
+def populate_site_cache():
+    base_url = os.environ.get('BASE_URL') or 'http://localhost:8000'
+    languages = ['en','pt','es','fr','id','hi']
+    index_pages = ['logbooks', 'stories', 'map', 'radio', 'radio-archive', 'contributors']
+    urls = []
+    for language in languages:
+        urls.append(f"{base_url}/{language}")
+    for language in languages:
+        for index_page in index_pages:
+            if index_page == 'logbooks':
+                if language == 'pt':
+                    index_page = 'diarios-de-bordo'
+                if language == 'es':
+                    index_page = 'bitacoras'
+                if language == 'fr':
+                    index_page = 'journaux-de-bord'
+                if language == 'id':
+                    index_page = 'buku-catatan'
+            if index_page == 'stories':
+                if language == 'pt':
+                    index_page = 'historias'
+                if language == 'es':
+                    index_page = 'historias'
+                if language == 'fr':
+                    index_page = 'histoires'
+                if language == 'id':
+                    index_page = 'cerita'
+            if index_page == 'map':
+                if language == 'pt':
+                    index_page = 'mapa'
+                if language == 'es':
+                    index_page = 'mapa'
+                if language == 'fr':
+                    index_page = 'carte'
+                if language == 'id':
+                    index_page = 'peta'
+            if index_page == 'contributors':
+                if language == 'pt':
+                    index_page = 'contribuidores'
+                if language == 'es':
+                    index_page = 'contribuidores'
+                if language == 'fr':
+                    index_page = 'contributeurs'
+                if language == 'id':
+                    index_page = 'kontributor'
+            urls.append(f"{base_url}/{language}/{index_page}")
+    
+    for url in urls:
+        response = requests.get(url) 
+        time.sleep(2)
+        print(url + " " + "OK" if str(response.ok) else "FAILED") 
+         
+    print(f"Site cache populated for {base_url}") 
--- smartforests/management/commands/ck_process_tasks.py
+++ smartforests/management/commands/ck_process_tasks.py
@@ -1,13 +1,15 @@
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
 from background_task.models import Task
-from logbooks.tasks import publish
-
+from logbooks.tasks import publish, populate_site_cache
+import os
 
 class Command(BaseCommand):
     help = "Run background tasks and publish scheduled pages"
 
     def handle(self, *args, **options):
+        interval = int(os.environ.get("CACHE_MIDDLEWARE_SECONDS", 600)) + 10
         print("Running background tasks")
         publish(repeat=Task.HOURLY / 2) # half-hourly
+        populate_site_cache(repeat=Task.HOURLY * interval / 3600) # every cache TTL + 10 seconds
         call_command("process_tasks")
