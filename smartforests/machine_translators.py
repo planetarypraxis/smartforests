a158f62e301f40fecde6bf7281abe5c25befd4cf
Joaquim d'Souza <joaquimds@gmail.com>
2024-07-10 13:25:55 +0200

    fix: google translate when content is large

--- smartforests/machine_translators.py
+++ smartforests/machine_translators.py
@@ -1,8 +1,12 @@
 from django.utils.module_loading import import_string
 from wagtail_localize.machine_translators.base import BaseMachineTranslator
-
+from wagtail_localize.machine_translators.google import GoogleCloudTranslator as BaseGoogleCloudTranslator
+from wagtail_localize.strings import StringValue
 
 class MultiTranslator(BaseMachineTranslator):
+    """
+    Switch between providers based on target locale.
+    """
     def __init__(self, options):
         super().__init__(options)
         self.translators = {
@@ -25,3 +29,41 @@ class MultiTranslator(BaseMachineTranslator):
         translator = self.get_translator(target_locale)
         self.display_name = translator.display_name
         return translator.can_translate(source_locale, target_locale)
+
+
+class GoogleCloudTranslator(BaseGoogleCloudTranslator):
+    """
+    Split up translation job into batches, to avoid going over the 30,000
+    codepoint (utf8 character) limit.
+    """
+    def translate(self, source_locale, target_locale, strings):
+        project_id = self.options["PROJECT_ID"]
+        location = self.options.get("LOCATION", "global")
+
+        batches = [[]]
+        current_batch_size = 0
+        for string in strings:
+            strlen = len(string.data)
+            if current_batch_size + strlen > 30000:
+                batches.append([])
+                current_batch_size = 0
+            batches[-1].append(string)
+            current_batch_size += strlen
+
+        translations = []
+        for batch in batches:
+            response = self.client.translate_text(
+                request={
+                    "parent": f"projects/{project_id}/locations/{location}",
+                    "contents": [string.data for string in batch],
+                    "mime_type": "text/html",
+                    "source_language_code": source_locale.language_code,
+                    "target_language_code": target_locale.language_code,
+                }
+            )
+            translations = translations + list(response.translations)
+
+        return {
+            string: StringValue(translation.translated_text)
+            for string, translation in zip(strings, translations)
+        }
