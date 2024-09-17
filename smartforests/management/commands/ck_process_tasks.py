9e283501e34ff6829429bc60a185cb8afadb3236
Joaquim d'Souza <joaquimds@gmail.com>
2024-09-17 19:09:00 +0200

    feat: add linkchecker daily task

--- smartforests/management/commands/ck_process_tasks.py
+++ smartforests/management/commands/ck_process_tasks.py
@@ -1,8 +1,10 @@
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
+from background_task import background
 from background_task.models import Task
 from logbooks.tasks import publish, populate_site_cache
 import os
+
 
 class Command(BaseCommand):
     help = "Run background tasks and publish scheduled pages"
@@ -10,6 +12,14 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         interval = int(os.environ.get("CACHE_MIDDLEWARE_SECONDS", 600)) + 10
         print("Running background tasks")
-        publish(repeat=Task.HOURLY / 2) # half-hourly
-        populate_site_cache(repeat=Task.HOURLY * interval / 3600) # every cache TTL + 10 seconds
+        publish(repeat=Task.HOURLY / 2)  # half-hourly
+        populate_site_cache(
+            repeat=Task.HOURLY * interval / 3600
+        )  # every cache TTL + 10 seconds
+        linkchecker(repeat=Task.DAILY)
         call_command("process_tasks")
+
+
+@background(schedule=5, remove_existing_tasks=True)
+def linkchecker():
+    call_command("linkchecker")
